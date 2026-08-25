4aa669fae17f042eab31503fcbb5bf28ea4f1940
4ngel-of-th3-d4rk <yoharroz@gmail.com>
2026-08-25 09:50:04 -0300

    Add files via upload

---
+++ profMilena/javaScript/exemplos,aritmetica.js
@@ -0,0 +1,34 @@
+
+
+let valor1 = 20
+let valor2 = 5
+console.log("exemplo 1")
+console.log(valor1 + valor2)
+console.log(valor1 * valor2)
+console.log(valor1 - valor2)
+console.log(valor1 / valor2)
+
+// aritmetica esses sao os comandos de soma, multiplicaçao, subtraçao e divisao
+
+console.log("exemplo 2")
+
+//exemplo 2
+ 
+console.log(10%3)
+
+console.log("exemplo 3")
+
+console.log(10>5); //true
+console.log(10<5); //false
+console.log(10>=5); //true
+console.log(10===5); //true
+console.log(10!==5); //true
+
+console.log("exemplo 4")
+console.log(10>=10&&20===20)
+
+console.log("exemplo 5")
+console.log(15>20||8<10)
+
+console.log("exemplo 6")
+console.log(!(20>10));
