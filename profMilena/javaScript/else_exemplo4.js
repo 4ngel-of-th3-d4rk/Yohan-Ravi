7fc54f0b132538f531123497506bb497a815b19b
4ngel-of-th3-d4rk <yoharroz@gmail.com>
2026-09-01 10:19:23 -0300

    Change alerts to console logs for password check

--- profMilena/javaScript/else_exemplo4.js
+++ profMilena/javaScript/else_exemplo4.js
@@ -1,17 +1,14 @@
 const readline = require("readline");
-
-
 const entrada = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 entrada.question("digite sua senha: ", (senha)=>{
-     senha = Number(senha)
 
 if (senha === "1234") {
-    alert("Acesso permitido!");
+   console.log ("Acesso permitido!");
 } else {
-    alert("Senha incorreta!");
+   console.log("Senha incorreta!");
 }
 
 })
